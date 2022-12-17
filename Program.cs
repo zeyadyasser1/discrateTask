58ff4228de3af374ce256821094944e41bc4825b
zeyadyasser1 <120785375+zeyadyasser1@users.noreply.github.com>
2022-12-17 14:14:03 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static bool isprfecet(int x)
+        {
+            int sum = 0;
+           
+            for (int i = 1; i < x; i++)
+            {
+                if (x % i == 0) sum = sum + i;
+
+            }
+            if (sum == x) return true;
+            return false;
+
+        }
+        static void Main(string[] args)
+        {
+            int y, z;
+            Console.WriteLine("enter the first number");
+            y = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter the second number");
+            z = int.Parse(Console.ReadLine());
+
+            for (int i = y; i <= z; i++)
+            {
+                if (isprfecet(i)) Console.WriteLine("{0},", i);
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
